1a974605d1e5c375eb67d3433dbe26eaca3bbf64
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-10-09 13:58:29 +0300

    Create twelthSQL.sql

---
+++ twelthSQL.sql
@@ -0,0 +1,3 @@
+INSERT INTO Employees (FirstName, LastName) VALUES ('Aidar', 'Mautaliev');
+INSERT INTO EmployeeTerritories (EmployeeID, TerritoryID) VALUES (12, 55113);
+INSERT INTO EmployeeTerritories (EmployeeID, TerritoryID) VALUES (12, 55439);
