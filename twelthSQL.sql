c9278efd8db299ce4b1c625f86a6ca93d4e2942c
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-10-09 14:03:39 +0300

    Update twelthSQL.sql

--- twelthSQL.sql
+++ twelthSQL.sql
@@ -1,3 +1,4 @@
 INSERT INTO Employees (FirstName, LastName) VALUES ('Aidar', 'Mautaliev');
 INSERT INTO EmployeeTerritories (EmployeeID, TerritoryID) VALUES (12, 55113);
 INSERT INTO EmployeeTerritories (EmployeeID, TerritoryID) VALUES (12, 55439);
+INSERT INTO Orders (CustomerID, EmployeeID) VAlUES ('OTTIK', 12);
