8377be22b3f907194ff02d019ad2322e854c8c7a
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-10-12 20:43:04 +0300

    Update ThirteenSQL.sql

--- ThirteenSQL.sql
+++ ThirteenSQL.sql
@@ -1,2 +1,5 @@
 UPDATE [Order Details] SET UnitPrice = 0.2 WHERE Quantity > 50;
 UPDATE Contacts SET Country = 'Russia', City = 'Piter' WHERE Country = 'Germany' AND City = 'Berlin';
+INSERT INTO Shippers (CompanyName) VALUES ('Yandex');
+INSERT INTO Shippers (CompanyName) VALUES ('Yandex1');
+DELETE FROM Shippers WHERE ShipperID > 3;
