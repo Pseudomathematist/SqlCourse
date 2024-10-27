58efc7fef0b8d599183a72e010289aaff150d8f3
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-10-27 23:58:10 +0300

    Update 18Lesson.sql

--- 18Lesson.sql
+++ 18Lesson.sql
@@ -1,4 +1,5 @@
 SELECT Dwarves.dwarf_id, Dwarves.name, Dwarves.age, Dwarves.profession, Dwarves.squad_id, Squads.name, Squads.mission FROM Dwarves, Squads WHERE (Dwarves.squad_id = Squads.squad_id);
 SELECT dwarf_id, name, age, profession FROM Dwarves WHERE profession = 'miner' AND squad_id IS NULL;
 SELECT task_id FROM Tasks WHERE priority in (SELECT MAX(priority) FROM Tasks));
-SELECT Dwarves.dwarf_id, (SELECT COUNT(owner_id) FROM Items WHERE Dwarves.dwarf_id = Items.owner_id) FROM Dwarves WHERE EXIST (SELECT owner_id FROM Items WHERE owner_id IS NOT NULL);
+SELECT Dwarves.dwarf_id, (SELECT COUNT(item_id) FROM Items WHERE Dwarves.dwarf_id = Items.owner_id) FROM Dwarves WHERE EXIST (SELECT owner_id FROM Items WHERE owner_id IS NOT NULL);
+SELECT squad_id, (SELECT COUNT(dwarf_id) FROM Dwarves WHERE Dwarves.squad_id = Squads.squad_id) FROM Squads;
