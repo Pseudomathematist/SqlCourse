04281d911db0be65d6fd07b50e9433fd10941afd
Pseudomathematist <mautaliev0aidar@gmail.com>
2024-11-02 21:32:10 +0300

    Create 20Lesson.sql

---
+++ 20Lesson.sql
@@ -0,0 +1,7 @@
+SELECT squad_id FROM Squads WHERE leader_id is NULL;
+SELECT dwarf_id FROM Dwarves WHERE age > 150 AND profession = 'warrior';
+SELECT dwarf_id FROM Dwarves WHERE dwarf_id in (SELECT owner_id FROM Items WHERE type = 'weapon');
+SELECT d.dwarf_id, COUNT(t.task_id) FROM Dwarves AS d JOIN Task AS t ON d.dwarf_id = t.owner_id
+GROUP BY d.dwarf_id, t.status;
+SELECT * FROM Tasks WHERE assigned_to in (SELECT dwarf_id FROM Dwarves WHERE squad_id in (SELECT squad_id WHERE name = 'Guardians'));
+SELECT * FROM Relationships;
